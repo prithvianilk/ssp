2c00f641c86f0045019daa6b139500508ee4edab
Ritesh Sapata <riteshsapata@gmail.com>
2022-09-07 21:48:03 +0530

    heap sort

--- 1/sort.py
+++ 1/sort.py
@@ -11,8 +11,31 @@ def selection_sort(array):
             if array[i] > array[j]:
                 swap(i, j, array)
 
+def heapify(array, N, i):
+    largest = i  
+    l = 2 * i + 1
+    r = 2 * i + 2
+ 
+    if l < N and array[largest] < array[l]:
+        largest = l
+ 
+    if r < N and array[largest] < array[r]:
+        largest = r
+ 
+    if largest != i:
+        array[i], array[largest] = array[largest], array[i]
+ 
+        heapify(array, N, largest)
 
 def merge_sort(array):
+    N = len(array)
+ 
+    for i in range(N//2 - 1, -1, -1):
+        heapify(array, N, i)
+ 
+    for i in range(N-1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
     raise NotImplementedError()
 
 
