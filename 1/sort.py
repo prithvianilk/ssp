beb7ef27c70d223b3d44d18e5ec733b424aa814b
rushab14 <79597429+rushab14@users.noreply.github.com>
2022-09-07 21:43:21 +0530

    Update sort.py

--- 1/sort.py
+++ 1/sort.py
@@ -21,4 +21,11 @@ def heap_sort(array):
 
 
 def insertion_sort(array):
-    raise NotImplementedError()
+    for i in range(1,len(array)):
+        pivot=array[i]
+        j=i-1
+        while j>=0 and pivot< array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = pivot
+    
