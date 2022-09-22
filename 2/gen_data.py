caa7d5cea58789cc096245e92f080a07eb34bb95
prithvianilk <thepirateking3d2y@gmail.com>
2022-09-22 17:02:15 +0530

    Change thread scaling to something more reasonable

--- 2/gen_data.py
+++ 2/gen_data.py
@@ -4,6 +4,6 @@ import os
 fnames = ['og_parallel_pi.cc', 'parallel_pi.cc']
 for fname in fnames:
     os.system(f'g++ -std=c++20 {fname}')
-    for i in range(1, 6):
-        thread_count = 10 ** i
+    for i in range(12):
+        thread_count = 2 ** i
         os.system(f'(time ./a.out {thread_count}) &> ./data/{fname}_{thread_count}.txt')
