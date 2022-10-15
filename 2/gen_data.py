66f1be621790d548a6fb2400e9bb47018bd544ec
prithvianilk <thepirateking3d2y@gmail.com>
2022-10-16 04:42:46 +0530

    Fix bug and rewrite metrics

--- 2/gen_data.py
+++ 2/gen_data.py
@@ -1,9 +1,9 @@
 import os
 
 
-fnames = ['og_parallel_pi.cc', 'parallel_pi.cc']
+fnames = ['og_parallel_pi', 'parallel_pi']
 for fname in fnames:
-    os.system(f'g++ -std=c++20 {fname}')
+    os.system(f'g++ -std=c++20 -o {fname} {fname}.cc')
 
 print(','.join(['#Threads'] + fnames))
 
@@ -12,7 +12,7 @@ for i in range(15):
     data = [str(thread_count)]
     for fname in fnames:
         path = f'./data/{fname}_{thread_count}.txt'
-        os.system(f'(time ./a.out {thread_count}) &> {path}')
+        os.system(f'(time ./{fname} {thread_count}) &> {path}')
         with open(path) as f:
             data.append(f.readlines()[2].split()[1][2: -1])
     print(','.join(data))
