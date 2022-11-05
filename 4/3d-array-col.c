3adacf1ad0437ada7682526c867d43bdd9f493a0
Ritesh Sapata <riteshsapata@gmail.com>
2022-11-05 08:43:21 +0530

    added column traversal

---
+++ 4/3d-array-col.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int traverse(int N,int* arr ){
+	int s=0;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			for(int k=0;k<N;k++){
+				s+=arr[i + j*N + k*N];
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int N = atoi(argv[1]);
+	int* arr = (int*)malloc(N*N*N*sizeof(int));
+	
+	for(int i=0;i<N*N*N;i++)
+		arr[i] = i+1;
+
+	clock_t t;
+	t=clock();
+	traverse(N,arr);
+	t=clock()-t;
+
+	double time_taken = ((double)t)/CLOCKS_PER_SEC;
+	printf("Traversal time for 3d-array-col N=%d: %f\n",N,time_taken);
+	free(arr);
+
+	return 0;
+}
