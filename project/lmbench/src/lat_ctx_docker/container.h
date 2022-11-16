c072d2bae7634eede23e484e38d0189bf21f7bb9
prithvianilk <thepirateking3d2y@gmail.com>
2022-11-16 14:40:27 +0000

    Mount common pipes volume
    - Mounted the ./pipes dir as a common volume for all containers

--- project/lmbench/src/lat_ctx_docker/container.h
+++ project/lmbench/src/lat_ctx_docker/container.h
@@ -3,6 +3,10 @@
 #include <fcntl.h>
 
 
+#define MAX_PATH_LEN 200
+#define MAX_PIPE_PATH_LEN 10 // path is of pattern "/pipes/1"
+#define MAX_CONTAINER_NAME_LEN 12 // name is of pattern "container_1"
+
 const char* sudo_path = "/usr/bin/sudo";
 const char* docker_path = "/usr/bin/docker";
 
@@ -11,15 +15,24 @@ void get_container_name(char* name, int id) {
         sprintf(name, "container_%d", id);
 }
 
+void get_volume_arg(char* volume_arg) {
+        char cwd[MAX_PATH_LEN];
+        getcwd(cwd, sizeof(cwd)) == NULL;
+        sprintf(volume_arg, "%s/pipes:/pipes/", cwd);
+}
+
 void run_container(int container_id) {
-        char read_pipe_path[10], write_pipe_path[10]; // path is of pattern "/pipes/1"
+        char read_pipe_path[MAX_PIPE_PATH_LEN], write_pipe_path[MAX_PIPE_PATH_LEN];
         sprintf(read_pipe_path, "/pipes/%d", container_id - 1);
         sprintf(write_pipe_path, "/pipes/%d", container_id);
 
-        char container_name[12]; // name is of pattern "container_1"
+        char container_name[MAX_CONTAINER_NAME_LEN]; 
         get_container_name(container_name, container_id);
 
-        char *args[] = {sudo_path, docker_path, "run", "--rm", "-d", "--name", container_name, "docker_proc", read_pipe_path, write_pipe_path, NULL};
+        char volume_arg[MAX_PATH_LEN];
+        get_volume_arg(volume_arg);
+
+        char *args[] = {sudo_path, docker_path, "run", "--rm", "-d", "-v", volume_arg, "--name", container_name, "docker_proc", read_pipe_path, write_pipe_path, NULL};
         execv(sudo_path, args);
 }
 
@@ -41,7 +54,7 @@ void run_containers(int container_count) {
 }
 
 void kill_container(int container_id) {
-        char container_name[12]; // name is of pattern "container_1"
+        char container_name[MAX_CONTAINER_NAME_LEN];
         get_container_name(container_name, container_id);
 
         char *args[] = {sudo_path, docker_path, "kill", container_name, NULL};
