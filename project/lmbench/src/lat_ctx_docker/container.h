a941f64ca734440ce7bce98ddf94655636e403c0
prithvianilk <thepirateking3d2y@gmail.com>
2022-11-16 11:17:51 +0000

    Refactor

--- project/lmbench/src/lat_ctx_docker/container.h
+++ project/lmbench/src/lat_ctx_docker/container.h
@@ -6,13 +6,18 @@
 const char* sudo_path = "/usr/bin/sudo";
 const char* docker_path = "/usr/bin/docker";
 
+
+void get_container_name(char* name, int id) {
+        sprintf(name, "container_%d", id);
+}
+
 void run_container(int container_id) {
         char read_pipe_path[10], write_pipe_path[10]; // path is of pattern "/pipes/1"
         sprintf(read_pipe_path, "/pipes/%d", container_id - 1);
         sprintf(write_pipe_path, "/pipes/%d", container_id);
 
         char container_name[12]; // name is of pattern "container_1"
-        sprintf(container_name, "container_%d", container_id);
+        get_container_name(container_name, container_id);
 
         char *args[] = {sudo_path, docker_path, "run", "--rm", "-d", "--name", container_name, "docker_proc", read_pipe_path, write_pipe_path, NULL};
         execv(sudo_path, args);
@@ -37,9 +42,9 @@ void run_containers(int container_count) {
 
 void kill_container(int container_id) {
         char container_name[12]; // name is of pattern "container_1"
-        sprintf(container_name, "container_%d", container_id);
+        get_container_name(container_name, container_id);
 
-        char *args[] = {sudo_path, docker_path, "container", "kill", container_name, NULL};
+        char *args[] = {sudo_path, docker_path, "kill", container_name, NULL};
         execv(sudo_path, args);
 }
 
